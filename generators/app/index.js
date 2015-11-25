c323aee22779865c6cab8e1bcf865d432d6c1d85
Michael Berman <me@scripti.st>
2015-11-25 22:04:59 +0100

    Fixed appname

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,10 +1,12 @@
 'use strict';
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
+var path = require('path');
 var yosay = require('yosay');
 
 module.exports = yeoman.generators.Base.extend({
 	prompting: function () {
+		this.appname = path.basename(this.destinationRoot());
 		this.log(yosay(
 			'Welcome to the ' + chalk.red('generator-slim-twig-scss-babel') + ' generator!'
 		));
