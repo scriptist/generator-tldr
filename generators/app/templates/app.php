022fc8dd8ed847bba7e461c05b5247b269461c06
Michael Berman <me@scripti.st>
2015-12-25 11:38:50 +0100

    Added maintenance mode

--- generators/app/templates/app.php
+++ generators/app/templates/app.php
@@ -1,5 +1,10 @@
 <?php
 
+if (file_exists('.maintenance')) {
+	http_response_code(503);
+	exit('<h3>Temporarily down for maintenance</h3><p>We are performing scheduled maintenance - we\'ll be back shortly.</p>');
+}
+
 require 'config.php';
 require 'app/app.php';
 
